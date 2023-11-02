9a89384314d9f6562505b5bc013c1c2d641618c4
nareswarara <nareswara.lintang@gmail.com>
2023-11-02 15:34:14 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <div>ewwewdfkjdsfklndfskl</div>
+        <div></div>
+        <div className="text-red-500">Hal</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
